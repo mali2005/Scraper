6e431fe7c1e95695fdf5f27fe032b7c0f8c46b5a
Mehmet Ali Aydınol <81029491+mali2005@users.noreply.github.com>
2022-02-14 13:35:24 +0300

    yess

--- ImageScraper.py
+++ ImageScraper.py
@@ -1,21 +1,22 @@
 import  requests
 from selenium import webdriver
+import  time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import shutil
-import threading
 import os
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome()
 
 
 def find_links(inputs,iters):
     links = []
+    word = inputs.replace(" ", "%20")
+    url = 'https://www.bing.com/images/search?q={}'.format(word)
+    driver.get(url)
+    time.sleep(2)
     for i in range(iters):
-        word = inputs.replace(" ", "%20")
-        url = 'https://www.bing.com/images/search?q={}'.format(word)
-        driver.get(url)
         driver.execute_script("window.scrollTo(0, {})".format(str(i*1000)))
         elements = driver.find_elements(By.CLASS_NAME, "mimg")
         for element in elements:
@@ -26,6 +27,7 @@ def find_links(inputs,iters):
                         pass
                     else:
                         links.append(src)
+    driver.close()
     return links
 
 
@@ -33,6 +35,18 @@ def download_images(urls, output_path, word):
     try:
         os.mkdir(output_path)
     except Exception:
+        pass
+
+    r = 0
+    i = 0
+    for url in urls:
+        response = requests.get(url, stream=True, verify=False)
+        with open('{}//{}_{}.png'.format(output_path, word, str(i)), 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+        i += 1
+
+
         pass
 
     r = 0
